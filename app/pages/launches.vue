ae1f6df68fcdf54b74fbecaf9041a97e2663fee5
Yvosh <cailayvosh@gmail.com>
2025-06-14 19:52:54 +0800

    Update launches.vue

--- app/pages/launches.vue
+++ app/pages/launches.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>Hello from Launches Page!</div>
+  <div>
+    Hello from Launches Page!
+  </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
-/* Add your styles here */
+/* Optional styles */
 </style>
